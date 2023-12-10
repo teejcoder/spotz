860bb7f48191ca3d1d2bae471ea053514978360a
Keej <96551689+teejcoder@users.noreply.github.com>
2023-12-10 14:16:45 +1100

    markers added

--- app/map/Map.js
+++ app/map/Map.js
@@ -1,30 +1,109 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native'
-import React, { Component } from 'react'
-import MapView from 'react-native-maps';
-import { router } from 'expo-router'
-import { useState } from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import MapView, { Marker } from 'react-native-maps';
+import { router } from 'expo-router';
+import * as Location from 'expo-location';
+
+let locationOfInterest = [
+  {
+    title: 'First Marker',
+    location: {
+      latitude: -37.8136,
+      longitude: 144.9631,
+    },
+    description: 'First Marker'
+  },
+  {
+    title: 'Second Marker',
+    location: {
+      latitude: -37.8003,
+      longitude: 144.9669,
+    },
+    description: 'Second Marker'
+  }
+]
 
 export default function Map() {
-  const [marker, setMarker] = useState('')
+  const [draggableMarker, setDraggableMarker] = useState({
+    latitude: -37.8120,
+    longitude: 144.9620
+  });
 
-    return (
-      <SafeAreaView style={styles.container}>
-        <MapView style={styles.map} 
+  const onRegionChange = (region) => {
+    console.log(region)
+  };
+
+  const showLocationOfInterest = () => {
+    return locationOfInterest.map((item, index) => {
+      return (
+        <Marker 
+          key={index}
+          coordinate={item.location}
+          title={item.title}
+          description={item.description}
+        />
+      )
+    })
+  };
+
+
+  // //USER LOCATION FOR PRECISE LOCATION
+  // const userLocation = async () => {
+  //   let {status} = await Location.requestForegroundPermissionsAsync();
+  //   if (!status){
+  //     setErrorMsg('Permission to access your Location denied by User');
+  //   }
+  //   let location = await Location.getCurrentPositionAsync({enableHighAccuracy: true });
+  //   setMapRegion({
+  //     latitude: location.coords.latitude,
+  //     longitude: location.coords.longitude,
+  //     latitudeDelta: 0.0922,
+  //     longitudeDelta: 0.0421,
+  //   });
+  //   console.log(location.coords.latitude, location.coords.longitude);
+  // }
+
+  // useEffect(() => {
+  //   userLocation();
+  // }, []);
+
+  return (
+    <View style={styles.container}>
+      <MapView 
+        style={styles.map} 
+        onRegionChange={onRegionChange}
         initialRegion={{
-          latitude: -37.8136,
-          longitude: 144.9631,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}/>
+            latitude: -37.8136,
+            latitudeDelta: 0.0922,
+            longitude: 144.9631,
+            longitudeDelta: 0.0144,
+        }}
+      >
+        {showLocationOfInterest()}
+        <Marker 
+          draggable
+          pinColor='#0000ff'
+          coordinate={draggableMarker} 
+          onDragEnd={(e) => setDraggableMarker(e.nativeEvent.coordinate)}
+        />
+      </MapView>
 
-        <Pressable onPress={() => router.push("/")} style={styles.pressable}>
+      <View style={styles.buttonContainer}>
+
+      {/* <Pressable onPress={userLocation}>
+          <Text>
+            Get Location
+          </Text>
+        </Pressable> */}
+
+        <Pressable onPress={() => router.push("/")}>
           <Text>
             Back
           </Text>
         </Pressable>
-      </SafeAreaView>
-    );
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -32,6 +111,16 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     textAlign: 'center',
+  },
+  getLocationPressable: {
+    width: '40%',
+    padding: 15,
+    margin: 5,
+    backgroundColor: '#B3E6B5',
+    borderWidth: 1,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   pressable: {
     width: '40%',
@@ -50,9 +139,13 @@ const styles = StyleSheet.create({
   map: {
     width: '100%',
     height: '100%',
-    backgroundColor: '#E6BAAC',
-    borderWidth: 1,
   },
+  buttonContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  }
 
   // mapComponent: {
   //   flex: 1,
